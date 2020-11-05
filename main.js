0a7f68da5e891fb30788c4facbd3b361a3245468
Dean Vo <deaanvo@gmail.com>
2020-11-05 13:28:19 -0800

    added transition time

--- main.js
+++ main.js
@@ -15,6 +15,32 @@ function turnCar(e) {
   }
 
   if (e.key === 'ArrowRight') {
-    $car.className = 'car';
+    $car.className = 'car rotateRight';
   }
 }
+
+// function turnCar(e) {
+//   var $car = document.querySelector('.car');
+
+//   if (e.key === 'ArrowDown') {
+//     $car.className = 'car rotateDown';
+//   } else if (e.key === 'ArrowLeft') {
+//     $car.className = 'car rotateLeft';
+//   } else if (e.key === 'ArrowUp') {
+//     $car.className = 'car rotateUp';
+//   } else if (e.key === 'ArrowRight') {
+//     $car.className = 'car rotateRight';
+//   }
+
+//   // if (e.key === 'ArrowLeft') {
+//   //   $car.className = 'car rotateLeft';
+//   // }
+
+//   // if (e.key === 'ArrowUp') {
+//   //   $car.className = 'car rotateUp';
+//   // }
+
+//   // if (e.key === 'ArrowRight') {
+//   //   $car.className = 'car rotateRight';
+//   // }
+// }
