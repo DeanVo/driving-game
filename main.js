fdd5a2420b1b978d13db13b0c0ee95199e865de0
Dean Vo <deaanvo@gmail.com>
2020-11-05 12:55:21 -0800

    car rotates with keydown event listener #2

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 document.addEventListener('keydown', turnCar);
 
 function turnCar(e) {
-  console.log(e);
+  var $car = document.querySelector('.car');
+  if (e.key === 'ArrowDown') {
+    $car.className = 'car rotateDown';
+  }
+
+  if (e.key === 'ArrowLeft') {
+    $car.className = 'car rotateLeft';
+  }
+
+  if (e.key === 'ArrowUp') {
+    $car.className = 'car rotateUp';
+  }
+
+  if (e.key === 'ArrowRight') {
+    $car.className = 'car';
+  }
 }
