675e4a1006361b2b50d3196fb2b00ac225f50f25
Dean Vo <deaanvo@gmail.com>
2020-11-05 14:51:58 -0800

    added data model that includes which way car is facing #2

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
+/* global data */
+
 document.addEventListener('keydown', turnCar);
 
 function turnCar(e) {
   var $car = document.querySelector('.car');
   if (e.key === 'ArrowDown') {
     $car.className = 'car rotateDown';
+    data.direction = 'south';
   }
 
   if (e.key === 'ArrowLeft') {
     $car.className = 'car rotateLeft';
+    data.direction = 'west';
   }
 
   if (e.key === 'ArrowUp') {
     $car.className = 'car rotateUp';
+    data.direction = 'north';
   }
 
   if (e.key === 'ArrowRight') {
     $car.className = 'car rotateRight';
+    data.direction = 'east';
   }
 }
